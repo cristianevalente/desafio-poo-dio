e9ec18fe9c443ad083098e0ed96e1326df4ece6c
cristiane <cristianevalente@hotmail.com>
2022-07-22 18:33:44 -0300

    Mentoria e Curso

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -16,13 +16,45 @@ public class Main {
         curso2.setDescricao("Descrição curso JS: excelente!!! ");
         curso2.setCargaHoraria(4);
 
+
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Enriquecedora!!!");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+  /*      System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+       Bootcamp bootcamp =new Bootcamp();
+       bootcamp.setNome("Bootcamp Java Developer");
+       bootcamp.setDescricao("Descrição Bootcamp Developer");
+       bootcamp.getConteudos().add(curso1);
+       bootcamp.getConteudos().add(curso2);
+       bootcamp.getConteudos().add(mentoria);
+
+       Dev devCristiane = new Dev();
+       devCristiane.setNome("Cristiane");
+       devCristiane.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Incritos Cristiane" + devCristiane.getConteudosInscritos());
+
+        devCristiane.progredir();
+        System.out.println("Conteúdos Incritos Cristiane" + devCristiane.getConteudosInscritos());
+       System.out.println("Conteúdos Concluídos Cristiane" + devCristiane.getConteudosConluidos());
+        System.out.println("XP: " + devCristiane.calcularXp());
+
+       Dev devBaethyr = new Dev();
+        devBaethyr.setNome("Baethyr");
+        devBaethyr.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Incritos" + devBaethyr.getConteudosInscritos());
+
+        devBaethyr.progredir();
+        devBaethyr.progredir();
+
+        System.out.println("Conteúdos Incritos" + devBaethyr.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos" + devBaethyr.getConteudosConluidos());
+        System.out.println("XP: " + devBaethyr.calcularXp());
+
     }
 }
